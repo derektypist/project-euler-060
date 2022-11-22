6a773d95f288572934b0d7203713677eb885639c
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-11-22 10:40:35 +0000

    Added isPrime and eratosthenes functions at script.js

--- js/script.js
+++ js/script.js
@@ -67,3 +67,38 @@ function primePairSets() {
 
     return soluce;
 }
+
+// Function to Return Array of Primes
+function eratosthenes(n) {
+    let detectPrimes = Array(n);
+    let primes = [];
+    detectPrimes[0] = false;
+    detectPrimes[1] = false;
+    for (let i=2;i<detectPrimes.length;i++) {
+        detectPrimes[i] = true;
+    }
+
+    for (let p=2;p<Math.sqrt(n);p++) {
+        if (detectPrimes[p]) {
+            for (let j=p*p;j<n;j+=p) {
+                detectPrimes[j] = false;
+            }
+        }
+    }
+
+    for (let i=0;i<n;i++) {
+        if (detectPrimes[i]) primes.push(i);
+    }
+
+    return primes;
+}
+
+// Function to check if number n is prime
+function isPrime(n) {
+    if (n<=3) return n>1;
+    if (n%2==0 || n%3==0) return false;
+    for (let i=5;i*i<=n;i+=6) {
+        if (n%i==0 || n%(i+2)==0) return false;
+    }
+    return true;
+}
