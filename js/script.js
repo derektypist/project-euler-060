ae3806b03406632c36cb27a543d2ae4bcebb118e
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-11-22 10:15:30 +0000

    Added function showSolution at script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Function to Show Solution
+function showSolution() {
+    // Set Up Variable
+    let txt = `Solution is ${primePairSets()}.<br>`;
+    // Display Solution in the Browser
+    document.getElementById("solution").innerHTML = txt;
+}
